1244376371f3caa8e79cf38d3219dc688aaa55f6
TOBY ROBLES CCOYCCOSI <10byrobles@gmail.com>
2019-02-04 14:59:22 -0500

    Agregar archivos de proyecto.

---
+++ EjemploRoles/Controllers/RoleController.cs
@@ -0,0 +1,65 @@
+﻿using EjemploRoles.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EjemploRoles.Controllers
+{
+    public class RoleController : Controller
+    {
+        //tutorial
+        ApplicationDbContext context;
+
+        public RoleController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+
+        // GET: Role
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+
+
+                if (!isAdminUser())
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var Roles = context.Roles.ToList();
+            return View(Roles);
+        }
+
+
+        public Boolean isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ApplicationDbContext context = new ApplicationDbContext();
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
